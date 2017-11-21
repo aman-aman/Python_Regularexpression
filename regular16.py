8352fbe67b87424892fb6c1dc6f6fe6933efd437
Aman Kumar Jha <31754652+aman-aman@users.noreply.github.com>
2017-11-21 22:04:39 +0530

    Add files via upload

---
+++ regular16.py
@@ -0,0 +1,5 @@
+import re
+xx="amanjdhdhdhghdbdhahsgsrfqamanxxxxshshssbsbdgdhdhsjsjsjshdehwququwyeyriwinanjsn"
+r1=re.findall(r"^\w+", xx)
+print(re.split(r'\s','we are splitting the words'))
+print(re.split(r's','split the words'))
